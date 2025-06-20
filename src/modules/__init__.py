a1f6dc39f89069ea387790eb6ebb3466ecb10013
Ary5272 <sim89892@gmail.com>
2025-06-19 20:45:37 -0400

    Create __init__.py

---
+++ src/modules/__init__.py
@@ -0,0 +1,8 @@
+# src/modules/__init__.py
+# This file makes the 'modules' directory a Python package and allows for easy imports.
+
+from .base_module import SpecializedCognitiveModule
+from .sfe import SensoryFusionEngine
+from .asm import AbstractSymbologyModule
+from .pse import PredictiveSimulationEngine
+from .csm import CreativeSynthesisModule
